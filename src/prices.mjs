c34c3fedd3cbf1c6a3c0644600cec1e114b60ca7
David Andrea <davejandrea@gmail.com>
2024-01-17 01:45:26 -0400

    finished first parallel refactor woohoo!

--- src/prices.mjs
+++ src/prices.mjs
@@ -23,11 +23,6 @@ function createApp(database) {
     res.json({ cost });
   });
 
-  function parseDate(dateString) {
-    if (dateString) {
-      return new Date(dateString.replaceAll("-", "/"));
-    }
-  }
 
   function parsePlainDate(dateString) {
     if (dateString) {
